7ac59f1b7772e6897cf7afe711dfa1ce6f2b7d80
jaygrinage <jonahgrinage@gmail.com>
2019-09-21 02:17:09 -0500

    Updates

---
+++ precache-manifest.bf0ec1d21976b2b66b422503c8787430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76dc21ee6950ba7dbe9e2c4a3b9ca063",
+    "url": "/br-test/index.html"
+  },
+  {
+    "revision": "a12d023e77624864b0c7",
+    "url": "/br-test/static/css/main.8314bc39.chunk.css"
+  },
+  {
+    "revision": "29bf14ad0e257e6035b9",
+    "url": "/br-test/static/js/2.0fe1c889.chunk.js"
+  },
+  {
+    "revision": "a12d023e77624864b0c7",
+    "url": "/br-test/static/js/main.e6c40eef.chunk.js"
+  },
+  {
+    "revision": "46ac2704f569a2c33441",
+    "url": "/br-test/static/js/runtime~main.73318701.js"
+  },
+  {
+    "revision": "315b2787a5a24db0b826856a88fd8837",
+    "url": "/br-test/static/media/location.315b2787.svg"
+  }
+]);
