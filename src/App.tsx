3c525cc12db68b058b0d8974dd90aed6cab36d91
jaygrinage <jonahgrinage@gmail.com>
2019-09-23 22:55:41 -0500

    removed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+// import { BrowserRouter, Route } from 'react-router-dom';
 import { RestaurantPage } from './modules/pages';
 
 const App: React.FC = () => {
   return (
-      <BrowserRouter>
-        <Route path='/' exact component={RestaurantPage}/>
-      </BrowserRouter>
+        <RestaurantPage />
   );
 }
 
